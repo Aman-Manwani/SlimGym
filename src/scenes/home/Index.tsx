cbcc954b6ed72da27d983a8eab875afe931055a7
Aman Manwani <aman04.manwani@gmail.com>
2023-06-03 00:01:40 +0530

    minor changes

---
+++ src/scenes/home/Index.tsx
@@ -0,0 +1,20 @@
+import { SelectedPage } from "@/shared/types";
+import useMediaQuery from "@/hooks/useMediaQueries";
+import ActionButton from "@/shared/ActionButton";
+import HomePageText from "@/assets/HomePageText.png";
+import HomePageGraphic from "@/assets/HomePageGraphic.png";
+import SponsorRedBull from "@/assets/SponsorRedBull.png";
+import SponsorForbes from "@/assets/SponsorForbes.png";
+import SponserFortune from "@/assets/SponserFortune.png";
+
+type Props = {
+  setSelectedPage: (value: SelectedPage) => void;
+};
+
+const Home = ({ setSelectedPage }: Props) => {
+  const isAboveMediumScreens = useMediaQuery("(min-width:1060px)");
+
+  return <div className="">Home</div>;
+};
+
+export default Home;
